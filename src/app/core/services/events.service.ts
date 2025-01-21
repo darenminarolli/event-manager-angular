13519af484eabc28347d59e7dd7d9e4bbac7f3cc
Daren Minarolli <darenminarolli@gmail.com>
2025-01-21 18:10:22 +0100

    fixes in events

--- src/app/core/services/events.service.ts
+++ src/app/core/services/events.service.ts
@@ -1,17 +1,40 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable, catchError, take, of } from 'rxjs';
 import { Event } from '../models/event.interface';
-import { Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root',
-})
+@Injectable()
 export class EventsService {
   private url = 'https://event-manager-api-ten.vercel.app/api/events';
+
+  data: Event[] | undefined;
+  isLoading = false;
+  hasError = false;
 
   constructor(private http: HttpClient) {}
 
   getAllEvents(): Observable<Event[]> {
     return this.http.get<Event[]>(this.url);
+  }
+
+  loadEvents(): void {
+    this.isLoading = true;
+    this.hasError = false;
+
+    this.getAllEvents()
+      .pipe(
+        take(1),
+        catchError(() => {
+          this.data = undefined;
+          this.isLoading = false;
+          this.hasError = true;
+          return of([]);
+        })
+      )
+      .subscribe((events: Event[]) => {
+        this.data = events;
+        this.isLoading = false;
+        this.hasError = false;
+      });
   }
 }
--- src/app/features/events/events.component.ts
+++ src/app/features/events/events.component.ts
@@ -1,29 +1,21 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ButtonComponent } from '../../shared/components/ui/button/button.component';
-import { EventCardComponent } from "../../shared/components/event-card/event-card.component";
+import { EventCardComponent } from '../../shared/components/event-card/event-card.component';
 import { EventsService } from '../../core/services/events.service';
-import { EventStateService } from '../../core/services/event-state.service';
-import { Event } from '../../core/models/event.interface';
-
-import { take } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-events',
   standalone: true,
-  imports: [
-    ButtonComponent, 
-    EventCardComponent
-  ],
+  imports: [CommonModule, ButtonComponent, EventCardComponent],
   providers: [EventsService],
   templateUrl: './events.component.html',
-  styleUrl: './events.component.css'
+  styleUrl: './events.component.css',
 })
 export class EventsPage implements OnInit {
-  perform = new EventStateService<Event[]>();
-
-  constructor(private eventService: EventsService){}
+  constructor(public eventsService: EventsService) {}
 
   ngOnInit(): void {
-    this.perform.load(this.eventService.getAllEvents());
-  }  
+    this.eventsService.loadEvents();
+  }
 }
