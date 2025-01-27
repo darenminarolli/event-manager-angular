2be054299558bb166466b7cf1eb73e95bec9d0af
Daren Minarolli <darenminarolli@gmail.com>
2025-01-27 14:38:06 +0100

    module added

--- src/app/features/events/events.component.ts
+++ src/app/features/events/events.component.ts
@@ -1,16 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { ButtonComponent } from '../../shared/components/ui/button/button.component';
-import { EventCardComponent } from '../../shared/components/event-card/event-card.component';
 import { EventsService } from '../../core/services/events.service';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-events',
-  standalone: true,
-  imports: [CommonModule, ButtonComponent, EventCardComponent],
-  providers: [EventsService],
+  standalone: false,
   templateUrl: './events.component.html',
-  styleUrl: './events.component.css',
+  styleUrls: ['./events.component.css'],
 })
 export class EventsPage implements OnInit {
   constructor(public eventsService: EventsService) {}
